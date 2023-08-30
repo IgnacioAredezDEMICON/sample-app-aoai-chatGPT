c34a5bde02b3062aea7c61184d87cd22be41d530
IgnacioAredezDEMICON <143659212+IgnacioAredezDEMICON@users.noreply.github.com>
2023-08-30 23:55:33 +0200

    Update Chat.tsx
    New chat description

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -544,8 +544,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Welcome to the TU Wien University Chat</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>I am an assistant ready to help you with your questions related to your registration and the necessary documents.</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
